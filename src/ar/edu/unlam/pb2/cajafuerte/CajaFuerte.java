e9a4a28e678c44524b6a57c77376b6d2b3aacbb4
Rocio Monaco <rociomonaco@gmail.com>
2022-04-25 11:33:43 -0300

    actualizacion de caja fuerte en clase

--- src/ar/edu/unlam/pb2/cajafuerte/CajaFuerte.java
+++ src/ar/edu/unlam/pb2/cajafuerte/CajaFuerte.java
@@ -4,13 +4,29 @@ public class CajaFuerte {
 
 	
 	private Boolean estaAbierta = true;
+	private Integer codigoDeApertura;
 
 	public Boolean estaAbierta() {
-		return estaAbierta;
+		return this.estaAbierta;
 	}
 
-	public void cerrar() {
-		this.estaAbierta = false;
+	//public void cerrar() {
+	//	this.estaAbierta = Boolean.FALSE;
+	//}
+
+	public void abrir(Integer codigoDeApertura) {
+		
+		if(this.codigoDeApertura.equals(codigoDeApertura)) {
+			this.estaAbierta =  Boolean.TRUE;			
+		}
+		
+	}
+
+	public void cerrar(Integer codigoDeApertura) {
+		this.codigoDeApertura = codigoDeApertura;
+		this.estaAbierta =  Boolean.FALSE;
+	
+		
 	}
 
 }
--- test/ar/edu/unlam/pb2/cajafuerte/CajaFuerteTest.java
+++ test/ar/edu/unlam/pb2/cajafuerte/CajaFuerteTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CajaFuerteTest {
+	private static final int CODIGO_DE_APERTURA = 123;
+
 	@Test
 	public void alCrearUnaCajaFuerteDeberiaEstarAbierta() {
 		// Preparacion - condiciones necesarias - (new) inicialiazaciones previos al
@@ -16,40 +18,66 @@ public class CajaFuerteTest {
 		// Contrastacion - entonces then
 		laCajaFuerteEstaAbierta(caja);
 	}
-	@Test 
-	public void alCerrarlaDeberiaQuedarCerrada() {
+	//@Test 
+	//public void alCerrarlaDeberiaQuedarCerrada() {
 		
-		CajaFuerte caja = dadoQueExisteUnaCajaFuerte();
+		//CajaFuerte caja = dadoQueExisteUnaCajaFuerte();
 		
-		cuandoCierroLaCajaFuerte(caja);
+		//cuandoCierroLaCajaFuerte(caja);
 		
-		entoncesLaCajaFuerteEstaCerrada(caja);
-	}
+		//entoncesLaCajaFuerteEstaCerrada(caja);
+	//}
+	
 	// Al abrir la caja fuerte con el codigo de serie deberia estar abierta
 	@Test
 	public void alAbrirLaCajaFuerteConElCodigoDeSerieDeberiaAbrirse() {
 		//perarapcion
-		CajaFuerte caja = dadoQueExisteUnaCajaFuerte();
+		CajaFuerte caja = dadoQueExisteUnaCajaFuerte(); //preparacion
 		
-			dadoQueCierroLaCajaFuerte(caja, codigoDeApertura);
+			dadoQueCierroLaCajaFuerte(caja, CODIGO_DE_APERTURA); //preparacion
 		
-		//cuando abro la caja fuerte la caja fuerte se abre ejecucion
-			cuandoAbroLaCajaFuerte(caja, codigoDeApertura);
+			//cuando abro la caja fuerte la caja fuerte se abre 
+			cuandoAbroLaCajaFuerte(caja, CODIGO_DE_APERTURA); //ejecucion
 			
-			//Costatacion
-			entoncesLaCajaFuerteSeAbre(caja);
-		
+			entoncesLaCajaFuerteSeAbre(caja); //Costrastacion
 	}
 	
+	@Test
+	public void alAbrirUnaCajaFuerteConElCodigoErroneoNoDeberiaAbrirse() {
+		//perarapcion
+		CajaFuerte caja = dadoQueExisteUnaCajaFuerte(); //preparacion
+		
+			dadoQueCierroLaCajaFuerte(caja, CODIGO_DE_APERTURA); //preparacion
+		
+			//cuando abro la caja fuerte la caja fuerte se abre 
+			cuandoAbroLaCajaFuerte(caja, CODIGO_DE_APERTURA + 5); //ejecucion
+			
+			entoncesLaCajaFuerteEstaCerrada(caja); //Costrastacion
+	}
+	
+	
+	private void cuandoAbroLaCajaFuerte(CajaFuerte caja, Integer codigoDeApertura) {
+		caja.abrir(codigoDeApertura);
+		
+	}
+	private void dadoQueCierroLaCajaFuerte(CajaFuerte caja, Integer codigoDeApertura) {
+		caja.cerrar(codigoDeApertura);
+		
+	}
+	private void entoncesLaCajaFuerteSeAbre(CajaFuerte caja) {
+		Assert.assertEquals(true, caja.estaAbierta());
+		
+	}
 	
 	private CajaFuerte dadoQueExisteUnaCajaFuerte() {
 		return new CajaFuerte();
 		
 	}
-	private void cuandoCierroLaCajaFuerte(CajaFuerte caja) {
-			caja.cerrar();
+	//private void cuandoCierroLaCajaFuerte(CajaFuerte caja) {
+		//	caja.cerrar();
 		
-	}
+	//}
+	
 	private void entoncesLaCajaFuerteEstaCerrada(CajaFuerte caja) {
 								//esperado, sobre obtenido
 		Assert.assertEquals(false, caja.estaAbierta());
